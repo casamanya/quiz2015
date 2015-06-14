cbd5d7de7d9152dc5a963c2fca9e76c59156838a
Ismael Garcia <igarcia.andorra@gmail.com>
2015-06-14 17:43:40 +0200

    logs de debug

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -21,6 +21,7 @@ exports.index = function(req, res){
 	if (search && search.length>0){		
 		search = search.trim().replace(' ','%');
 		search = '%' + search + '%';
+		console.log('query de preguntas like ' + search);
 		models.Quiz.findAll({where: ["pregunta like ?", search]}).then(function (quizes){
 			res.render('quizes/index.ejs', {quizes:quizes, search: req.query.search});
 		});
