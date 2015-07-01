1dd9d4e3434cc313c6f112761f6b4bd4ea886f5d
Ismael Garcia <igarcia.andorra@gmail.com>
2015-07-01 09:42:09 +0200

    borrar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -110,3 +110,13 @@ exports.update = function(req, res) {
 		}
 	);
 };
+
+exports.destroy = function (req, res, next ){
+	req.quiz.destroy().then(
+		function(){
+			res.redirect('/quizes');
+		}		
+	).catch(
+		function(error){ next(error);}
+	);
+};
